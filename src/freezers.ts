a5d4ab99d18a89348861e8f14f5938a8dd574699
TubbyStubby <tubbystubby2@gmail.com>
2023-08-06 20:16:44 +0530

    feat: Frozen and DeepFrozen types (#3)
    * added Frozen and DeepFrozen types
    and fn is not frozen anymore
    
    * change to pre release and major upgrade
    
    * refactor: combine handler and freezer files
    
    * minor bump for refactor
    
    * Enter prerelease mode and version packages
    
    * make conifg public
    
    * doc update
    
    * exit pre release

--- src/freezers.ts
+++ src/freezers.ts
@@ -1,29 +1,70 @@
-import { mutationHandler } from "./handlers";
+import { ConstConstError } from "./errors"
 
-export function freeze<T>(obj: T): Readonly<T> {
-    if(obj == undefined) return obj;
-    if(typeof obj != "function" && typeof obj != "object") return obj;
-    if(Object.isFrozen(obj)) return obj;
-    Object.freeze(obj);
-    return new Proxy(obj, mutationHandler);
+export type Frozen<T> = 
+    T extends NonObject ? T :
+    T extends object ? FrozenObject<T> :
+    T
+
+export type DeepFrozen<T> =
+    T extends NonObject ? T :
+    T extends ReadonlyArray<infer R> ? DeepFrozenArray<R> :
+    T extends object ? DeepFrozenObject<T> :
+    T
+
+type NonObject =
+    | null
+    | undefined
+    | string
+    | number
+    | boolean
+    | symbol
+    | bigint
+    | AnyFunction
+
+type AnyFunction = (...args: unknown[]) => unknown
+
+type FrozenObject<T> = {
+    readonly [K in keyof T]: T[K];
 }
 
-export function deepFreeze<T>(obj: T): Readonly<T> {
-    if(obj == undefined) return obj;
-    if(typeof obj != "function" && typeof obj != "object") return obj;
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface DeepFrozenArray<T> extends ReadonlyArray<DeepFrozen<T>> {}
+
+type DeepFrozenObject<T> = {
+    readonly [K in keyof T]: DeepFrozen<T[K]>;
+}
+
+export function freeze<T>(obj: T): Frozen<T> {
+    if(obj == undefined) {
+        return obj as Frozen<T>;
+    } else if(typeof obj != "object") {
+        return obj as Frozen<T>;
+    } else {
+        if(Object.isFrozen(obj)) {
+            return obj as Frozen<T>;
+        } else {
+            Object.freeze(obj);
+            return new Proxy(obj, mutationHandler) as Frozen<T>;
+        }
+    }
+}
+
+export function deepFreeze<T>(obj: T): DeepFrozen<T> {
+    if(obj == undefined) return obj as DeepFrozen<T>;
+    if(typeof obj != "object") return obj as DeepFrozen<T>;
     const wm = new WeakMap();
     return deepFreezer(obj, wm);
 }
 
-function deepFreezer<T>(obj: T, seenObj: WeakMap<object, Readonly<T>>): Readonly<T> {
-    if(obj == undefined) return obj;
-    if(typeof obj != "function" && typeof obj != "object") return obj;
-    if(Object.isFrozen(obj)) return obj;
+function deepFreezer<T>(obj: T, seenObj: WeakMap<object, unknown>): DeepFrozen<T> {
+    if(obj == undefined) return obj as DeepFrozen<T>;
+    if(typeof obj != "object") return obj as DeepFrozen<T>;
+    if(Object.isFrozen(obj)) return obj as DeepFrozen<T>;
     let proxyObj;
     if(seenObj.has(obj)) {
         const seenRef = seenObj.get(obj);
         if(!seenRef) throw new Error("Object got GCed before freezing completed!");
-        return seenRef;
+        return seenRef as DeepFrozen<T>;
     } else {
         proxyObj = new Proxy(obj, mutationHandler);
         seenObj.set(obj, proxyObj);
@@ -31,15 +72,25 @@ function deepFreezer<T>(obj: T, seenObj: WeakMap<object, Readonly<T>>): Readonly
     if(obj instanceof Array) {
         for(let i = 0; i < obj.length; i++) {
             const val = Reflect.get(obj, i);
-            Reflect.set(obj, i, deepFreezer((val as any), seenObj));
+            Reflect.set(obj, i, deepFreezer(val, seenObj));
         }
     } else {
         const keys = Reflect.ownKeys(obj);
         for(const key of keys) {
             const val = Reflect.get(obj, key);
-            Reflect.set(obj, key, deepFreezer((val as any), seenObj));
+            Reflect.set(obj, key, deepFreezer(val, seenObj));
         }
     }
     Object.freeze(obj);
     return proxyObj;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const mutationHandler: ProxyHandler<any> = {
+    set(_, prop, val) {
+        throw ConstConstError.newSetError(prop, val);
+    },
+    deleteProperty(_, prop) {
+        throw ConstConstError.newDeleteError(prop);
+    }
 }
